cd43366ac0d8f335d17acf17875cf43d1d2e6c33
tjibon <41022171+tjibon@users.noreply.github.com>
2023-01-08 14:48:11 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,210 @@
+// ..............Custom JavaScript Code.................
+
+
+ /******* */
+(function ($) {
+  "use strict";
+  jQuery(document).ready(function ($) {
+
+
+
+   
+         //model_lsit
+     $('.model-list').owlCarousel({  
+        items: 4,
+        autoplay: true,
+        autoplayTimeout:5000,
+        smartSpeed: 1000,
+        loop: true,
+        margin: 20,
+        dots: true,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+           0:{
+               items:1,
+               dots: true,
+               margin:20,
+              
+
+           },
+           500:{
+            items:2,
+            dots: true,
+            margin:20,
+           
+
+            },
+           600:{
+               items:3,
+               dots: true,
+               margin:20,
+               
+           },
+           1000:{
+               items:4,
+               loop:false,
+               dots: false,
+               mouseDrag: false,
+              
+            
+           }
+        }
+      
+     });
+      
+      
+      
+      
+      
+      
+      //profile_list
+     $('.profile-list').owlCarousel({  
+        items: 4,
+        autoplay: true,
+        autoplayTimeout:5000,
+        smartSpeed: 1000,
+        loop: true,
+        margin: 20,
+        dots: true,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+           0:{
+               items:1,
+               dots: true,
+               margin:20,
+              
+
+           },
+           500:{
+            items:2,
+            dots: true,
+            margin:20,
+           
+
+            },
+           600:{
+               items:3,
+               dots: true,
+               margin:20,
+               
+           },
+           1000:{
+               items:4,
+               loop:false,
+               dots: false,
+               mouseDrag: false,
+              
+            
+           }
+        }
+      
+    });
+    //collaboration_box
+   
+     //collaboration_box
+     $('.collaboration_box-list').owlCarousel({  
+        items: 4,
+        autoplay: true,
+        autoplayTimeout:5000,
+        smartSpeed: 1000,
+        loop: true,
+        margin: 20,
+        dots: true,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+           0:{
+               items:1,
+               dots: true,
+               margin:5,
+              
+
+           },
+           500:{
+            items:2,
+            dots: true,
+            margin:5,
+           
+
+            },
+           600:{
+               items:2,
+               dots: true,
+               
+           },
+           1000:{
+               items:4,
+               loop:true,
+               dots: true,
+              
+            
+           }
+        }
+      
+    });
+    //collaboration_box
+    $('.testimonial-list').owlCarousel({  
+      items: 1,
+      autoplay: true,
+      autoplayTimeout:5000,
+      loop: true,
+      dots:true,
+      nav:false,
+      responsiveClass:true,
+      responsive:{
+         0:{
+             items:1,
+             nav:false,
+        
+
+         },
+         600:{
+             items:1,
+             nav:false
+         },
+         1000:{
+             items:1,
+             loop:true,
+             nav:false,
+         }
+      }
+    
+  });
+
+    //faq
+    $(".faq").accordion({
+        questionClass: '.question',
+        answerClass: '.answer',
+        itemClass: '.item',
+        animationDuration: 400,
+    });
+
+
+
+  });
+
+    $(".default_option").click(function(){
+        $(".dropdown ul").addClass("active");
+    });
+  
+    $(".dropdown ul li").click(function(){
+        var text = $(this).text();
+        $(".default_option").text(text);
+        $(".dropdown ul").removeClass("active");
+    });
+
+    // search -filter
+    $('#ad-filter').jq_collapse({
+        //...
+    });
+  
+
+
+
+
+
+
+
+}(jQuery));
